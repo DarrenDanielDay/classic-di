70cf5490441a3f93a6df40e896e7cf72247d2c4b
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-11-19 23:51:55 +0800

    feat: ship `jest` with `ECMAScript Module`

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { JestConfigWithTsJest } from "ts-jest/dist/types";
-import tsJestPresets from "ts-jest/presets";
-const config: JestConfigWithTsJest = {
-  transform: {
-    ".*\\.tsx?": ["ts-jest", tsJestPresets.jsWithTsESM],
-  },
-  testMatch: ["./**/?(*.)+(spec|test).[t]s?(x)"],
-};
-export default config;
